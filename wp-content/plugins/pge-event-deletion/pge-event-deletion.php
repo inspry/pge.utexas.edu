c368df81c47d602f5887293d6e6e2059180c26e6
Grayson Gantek <grayson@inspry.com>
2024-12-02 12:59:18 -0500

    Minor refactoring, added function existance checks

--- wp-content/plugins/pge-event-deletion/pge-event-deletion.php
+++ wp-content/plugins/pge-event-deletion/pge-event-deletion.php
@@ -14,55 +14,58 @@ if ( ! function_exists( 'add_action' ) ) {
 	exit();
 }
 
-
-/**
- * Function to loop through the events and remove the ones that have a date passed today.
- */
-function pge_event_deletion_run() {
-	// Get today's date.
-	$date_now = strtotime( 'now' );
-
-	// Get the events. Only get events in "university event" tag.
-	$events = get_posts(
-		array(
-			'numberposts' => -1,
-			'post_type'   => 'event',
-			'post_status' => 'publish',
-			'tax_query'   => array(
-				array(
-					'taxonomy' => 'event-type',
-					'field'    => 'slug',
-					'terms'    => array( 'university-event' ),
-					'operator' => 'IN',
+if (!function_exists('pge_event_deletion_run')) {
+	/**
+	 * Function to loop through the events and remove the ones that have a date passed today.
+	 */
+	function pge_event_deletion_run() {
+		// Get today's date.
+		$date_now = strtotime( 'now' );
+	
+		// Get the events. Only get events in "university event" tag.
+		$events = get_posts(
+			array(
+				'numberposts' => -1,
+				'post_type'   => 'event',
+				'post_status' => 'publish',
+				'tax_query'   => array(
+					array(
+						'taxonomy' => 'event-type',
+						'field'    => 'slug',
+						'terms'    => array( 'university-event' ),
+						'operator' => 'IN',
+					),
 				),
-			),
-		)
-	);
-
-	// Make sure the events arrat is not empty.
-	if ( ! empty( $events ) ) {
-		// Loop through the events.
-		foreach ( $events as $event ) {
-			// Get the date of this event.
-			$event_date = get_field( 'date', $event->ID, true );
-			$event_date = strtotime( $event_date );
-
-			// Check if the event has passed.
-			if ( $date_now > $event_date ) {
-				// Move this event to the trash.
-				wp_trash_post( $event->ID );
+			)
+		);
+	
+		// Make sure the events arrat is not empty.
+		if ( ! empty( $events ) ) {
+			// Loop through the events.
+			foreach ( $events as $event ) {
+				// Get the date of this event.
+				$event_date = get_field( 'date', $event->ID, true );
+				$event_date = strtotime( $event_date );
+	
+				// Check if the event has passed.
+				if ( $date_now > $event_date ) {
+					// Move this event to the trash.
+					wp_trash_post( $event->ID );
+				}
 			}
 		}
 	}
+	add_action( 'pge_event_deletion', 'pge_event_deletion_run' );
 }
-add_action( 'pge_event_deletion', 'pge_event_deletion_run' );
 
-/**
- * Schedule the cron to run once daily.
- */
-function pge_event_deletion_schedule_cron() {
-	if ( ! wp_next_scheduled( 'pge_event_deletion' ) ) {
-		wp_schedule_event( time(), 'daily', 'pge_event_deletion' );
+if (!function_exists('pge_event_deletion_schedule_cron')) {
+	/**
+	 * Schedule the cron to run once daily.
+	 */
+	function pge_event_deletion_schedule_cron() {
+		if ( ! wp_next_scheduled( 'pge_event_deletion' ) ) {
+			wp_schedule_event( time(), 'daily', 'pge_event_deletion' );
+		}
 	}
+	add_action( 'wp', 'pge_event_deletion_schedule_cron' );
 }
-add_action( 'wp', 'pge_event_deletion_schedule_cron' );
--- wp-content/themes/inspry-theme/functions.php
+++ wp-content/themes/inspry-theme/functions.php
@@ -1,32 +1,34 @@
 <?php
 
-/**
- * Enqueue child styles.
- */
-function child_enqueue_styles()
-{
-	wp_enqueue_style('child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100);
-}
-
-// add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
-
-/**
- * Add custom functions here
- */
 if (!function_exists('inspry_year_shortcode_handler')) {
+    /**
+     * Handles shortcode that displays the year.
+     * 
+     * Usage: `[year]`.
+     * 
+     * @return string Shortcode markup.
+     */
 	function inspry_year_shortcode_handler()
 	{
 		return date('Y');
 	}
+    add_shortcode('year', 'inspry_year_shortcode_handler');
 }
-add_shortcode('year', 'inspry_year_shortcode_handler');
 
-add_filter('the_content', 'force_br_paragraph_tags_for_page', 20);
-function force_br_paragraph_tags_for_page($content) {
-    // Check if it's the specific page or the archive page for the post type
-    if (is_page(2331)) {
-        // Apply wpautop only for the specific page or post type archive
-        $content = wpautop($content);
+if (!function_exists('force_br_paragraph_tags_for_page')) {
+    /**
+     * Runs `wpautop` on content for specific pages.
+     * 
+     * @return string Modified content.
+     */
+    function force_br_paragraph_tags_for_page($content) {
+        // Check if it's the specific page or the archive page for the post type
+        if (is_page(2331)) {
+            // Apply wpautop only for the specific page or post type archive
+            $content = wpautop($content);
+        }
+
+        return $content;
     }
-    return $content;
+    add_filter('the_content', 'force_br_paragraph_tags_for_page', 20);
 }
